2a77cb67ad5c7b7beb695ecf582e61c476ef96f9
jexordev <seger.dustin@gmail.com>
2017-10-17 16:58:08 -0500

    added thymeleaf

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -3,13 +3,14 @@ package com.example.demo.controllers;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
 	@RequestMapping("/home")
-	public String home() {
-		//model.addAttribute("message", "adios");
-		//return model;
-		return "here";
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("message", "adios");
+		return mv;		
 	}
 }
