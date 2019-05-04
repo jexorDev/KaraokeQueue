9657fd04336a6a9194cf99f4b17b3a0591e99059
Dustin S <seger.dustin@gmail.com>
2019-05-04 15:48:38 -0500

    vote from kiosk working. need to test

--- src/main/java/com/example/demo/controllers/VotesController.java
+++ src/main/java/com/example/demo/controllers/VotesController.java
@@ -17,8 +17,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.models.SongRequest;
 import com.example.demo.models.SongRequestDao;
@@ -35,14 +37,44 @@ public class VotesController {
 	private UserDao userDao;
 
 	@Autowired
+	private UserRoleDao userRoleDao;
+	
+	@Autowired
 	private SongRequestDao songRequestDao;
 
 	@RequestMapping(value="/vote", method=RequestMethod.GET)
 	public ModelAndView index()
-	{
-		ModelAndView mv = new ModelAndView("votes/index");
+	{		
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User votingUser = userDao.findByUsername(auth.getName());
+		Boolean isKiosk = userRoleDao.findByUsernameAndRole(auth.getName(), "ROLE_KIOSK") != null;
+		ModelAndView mv = new ModelAndView("votes/index");
+		
+		if (!isKiosk)
+		{
+			User votingUser = userDao.findByUsername(auth.getName());
+			List<SongRequest> songsToVoteOn = songRequestDao.findAllByIsCompleteOrderBySequenceAscIdAsc(true)
+					.stream()
+					.filter(x -> x.getUser().getUsername() != votingUser.getUsername())
+					.collect(Collectors.toList());
+			Map<User, List<SongRequest>> userSongMap = songsToVoteOn
+					.stream()
+					.collect(Collectors.groupingBy(SongRequest::getUser, Collectors.toList()));
+			
+			mv.addObject("userSongMap", userSongMap);
+			mv.addObject("votingUser", votingUser);	
+		}
+		
+		mv.addObject("isKiosk", isKiosk);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/vote/{kiosk-user-id}", method=RequestMethod.GET)
+	public ModelAndView index(@PathVariable(value="kiosk-user-id") String userIdFromKiosk)
+	{			
+		ModelAndView mv = new ModelAndView("fragments/voteList");
+		
+		User votingUser = userDao.findById(Long.parseLong(userIdFromKiosk));
 		List<SongRequest> songsToVoteOn = songRequestDao.findAllByIsCompleteOrderBySequenceAscIdAsc(true)
 				.stream()
 				.filter(x -> x.getUser().getUsername() != votingUser.getUsername())
@@ -50,22 +82,25 @@ public class VotesController {
 		Map<User, List<SongRequest>> userSongMap = songsToVoteOn
 				.stream()
 				.collect(Collectors.groupingBy(SongRequest::getUser, Collectors.toList()));
-				
+		
 		mv.addObject("userSongMap", userSongMap);
-		mv.addObject("votingUser", votingUser);
+		mv.addObject("votingUser", votingUser);	
+		mv.addObject("isKiosk", true);
 		
 		return mv;
 	}
 
 	@RequestMapping(value = "/vote/{id}", method = RequestMethod.POST)
-	public ModelAndView process(@PathVariable("id") String id) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User votingUser = userDao.findByUsername(auth.getName());
+	public ModelAndView process(@PathVariable("id") String id, @RequestParam(value="voting-user-id", required=false) String votingUserId, RedirectAttributes redirectAttributes) {
+		User votingUser = userDao.findById(Long.parseLong(votingUserId));
 		User votedForUser = userDao.findById(Long.parseLong(id));
 
 		votingUser.setVote(votedForUser);
 		userDao.save(votingUser);
-
+		
+		redirectAttributes.addFlashAttribute("strongMessage", "Vote Casted");
+		redirectAttributes.addFlashAttribute("mainMessage", "You can change your vote at any time by revisting the Vote page.");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-primary");
 		return new ModelAndView("redirect:/home");
 	}
 	
